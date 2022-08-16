9702f20e9604573d670a48b746cea6d8651c87cf
David Goffredo <dmgoffredo@gmail.com>
2022-08-16 08:46:06 -0400

    TODO: amend: header parsing, untested

--- src/curl.h
+++ src/curl.h
@@ -2,9 +2,13 @@
 
 #include <curl/curl.h>
 
+#include <algorithm>
+#include <cctype>
 #include <cstddef>
+#include <iterator>
 #include <mutex>
 #include <sstream>
+#include <string_view>
 #include <thread>
 #include <unordered_map>
 #include <unordered_set>
@@ -32,8 +36,13 @@ class Curl : public HTTPClient {
   };
 
   void run();
-  static std::size_t on_read_header(char *data, std::size_t, std::size_t length, void *user_data);
-  static std::size_t on_read_body(char *data, std::size_t, std::size_t length, void *user_data);
+  static std::size_t on_read_header(char *data, std::size_t, std::size_t length,
+                                    void *user_data);
+  static std::size_t on_read_body(char *data, std::size_t, std::size_t length,
+                                  void *user_data);
+  static bool is_non_whitespace(unsigned char);
+  static char to_lower(unsigned char);
+  static std::string_view trim(std::string_view);
 
  public:
   Curl();
@@ -70,7 +79,7 @@ inline std::optional<Error> Curl::post(std::string_view URL,
   request->request_body = std::move(body);
   request->on_response = std::move(on_response);
   request->on_error = std::move(on_error);
-  
+
   CURL *handle = curl_easy_init();
   // TODO: error handling
   curl_easy_setopt(handle, CURLOPT_ERRORBUFFER, request->error_buffer);
@@ -82,43 +91,70 @@ inline std::optional<Error> Curl::post(std::string_view URL,
   curl_easy_setopt(handle, CURLOPT_WRITEFUNCTION, &on_read_body);
   curl_easy_setopt(handle, CURLOPT_WRITEDATA, request.get());
   // TODO
-  
+  (void)URL;
+  (void)set_headers;
+
   return std::nullopt;  // TODO
 }
 
-inline std::size_t Curl::on_read_header(char *data, std::size_t, std::size_t length, void *user_data) {
-    const auto request = static_cast<Request*>(user_data);
-    // TODO:
-    //    "    Foo-Bar:   thingy, thingy, thing   "
-    //    -> {"foo-bar", "thingy, thingy, thing"}
-    //
-    // TODO: Look out!
-    //
-    // > For an HTTP transfer, the status line and the blank line preceding the
-    // > response body are both included as headers and passed to this
-    // > function. 
-    //
-    // https://curl.se/libcurl/c/CURLOPT_HEADERFUNCTION.html
-    //
-    // Positions:
-    // A. First non-whitespace
-    // B. First colon
-    // C. Last non-whitespace
-    // D. First non-whitespace after colon
-    //
-    // Corner case:
-    // - C = B,  D = end
-    //     - The header value is empty (or all whitespace).
-    //
+inline std::size_t Curl::on_read_header(char *data, std::size_t,
+                                        std::size_t length, void *user_data) {
+  const auto request = static_cast<Request *>(user_data);
+  // The idea is:
+  //
+  //         "    Foo-Bar  :   thingy, thingy, thing   \r\n"
+  //    -> {"foo-bar", "thingy, thingy, thing"}
+  //
+  // There isn't always a colon.  Inputs without a colon can be ignored:
+  //
+  // > For an HTTP transfer, the status line and the blank line preceding the
+  // > response body are both included as headers and passed to this
+  // > function.
+  //
+  // https://curl.se/libcurl/c/CURLOPT_HEADERFUNCTION.html
+  //
+
+  const char *const begin = data;
+  const char *const end = begin + length;
+  const char *const colon = std::find(begin, end, ':');
+  if (colon == end) {
     return length;
+  }
+
+  const auto key = trim(std::string_view(begin, colon - begin));
+  const auto value = trim(std::string_view(colon + 1, end - (colon + 1)));
+
+  std::string key_lower;
+  key_lower.reserve(key.size());
+  std::transform(key.begin(), key.end(), std::back_inserter(key_lower),
+                 &to_lower);
+
+  request->response_headers_lower.emplace(std::move(key_lower), value);
+  return length;
 }
 
-inline std::size_t Curl::on_read_body(char *data, std::size_t, std::size_t length, void *user_data) {
-    const auto request = static_cast<Request*>(user_data);
-    if (!request->response_body.write(data, length)) {
-        return -1;  // Any value other than `length` will do.
-    }
-    return length;
+inline bool Curl::is_non_whitespace(unsigned char ch) {
+  return !std::isspace(ch);
+}
+
+inline char Curl::to_lower(unsigned char ch) { return std::tolower(ch); }
+
+inline std::string_view Curl::trim(std::string_view source) {
+  const auto first_non_whitespace =
+      std::find_if(source.begin(), source.end(), &is_non_whitespace);
+  const auto after_last_non_whitespace =
+      std::find_if(source.rbegin(), source.rend(), &is_non_whitespace).base();
+  const auto trimmed_length = after_last_non_whitespace - first_non_whitespace;
+  return std::string_view(first_non_whitespace, trimmed_length);
+}
+
+inline std::size_t Curl::on_read_body(char *data, std::size_t,
+                                      std::size_t length, void *user_data) {
+  const auto request = static_cast<Request *>(user_data);
+  if (!request->response_body.write(data, length)) {
+    return -1;  // Any value other than `length` will do.
+  }
+  return length;
 }
 
 inline void Curl::run() {
