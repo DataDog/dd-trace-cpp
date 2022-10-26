68cc5554fc39c49721ee19627e00f22e98d2d03c
David Goffredo <david.goffredo@datadoghq.com>
2022-10-26 16:14:10 -0400

    document collector.h

--- src/datadog/collector.h
+++ src/datadog/collector.h
@@ -1,5 +1,15 @@
 #pragma once
 
+// This component provides an interface, `Collector`, to which spans of
+// completed trace segments can be sent.
+//
+// `DatadogAgent`, defined in `datadog_agent.h`, implements `Collector` by
+// serializing the spans and sending them to a Datadog Agent.
+//
+// As a result of `send`ing spans to a `Collector`, the `TraceSampler` might be
+// adjusted to increase or decrease the rate at which traces are kept.  See the
+// `response_handler` parameter to `Collector::send`.
+
 #include <memory>
 #include <optional>
 #include <vector>
@@ -14,6 +24,10 @@ class TraceSampler;
 
 class Collector {
  public:
+  // Submit ownership of the specified `spans` to the collector.  If the
+  // collector delivers a response relevant to trace sampling, reconfigure the
+  // sampler using the specified `response_handler`.  Return an error if one
+  // occurs.
   virtual Expected<void> send(
       std::vector<std::unique_ptr<SpanData>>&& spans,
       const std::shared_ptr<TraceSampler>& response_handler) = 0;
