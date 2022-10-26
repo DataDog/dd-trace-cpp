13e0ef50d401eb7a3e627fb1b84e2e774628c727
David Goffredo <david.goffredo@datadoghq.com>
2022-10-26 18:26:55 -0400

    document environment.h, and use the preprocessor

--- src/datadog/environment.h
+++ src/datadog/environment.h
@@ -1,5 +1,19 @@
 #pragma once
 
+// This component provides a registry of all environment variables that can be
+// used to configure this library.
+//
+// Each `enum Variable` denotes an environment variable.  The enum value names
+// are the same as the names of the environment variables.
+//
+// `variable_names` is an array of the names of the environment variables. Nginx
+// uses `variable_names` as an allow list of environment variables to forward to
+// worker processes.
+//
+// `name` returns the name of a specified `Variable`.
+//
+// `lookup` retrieves the value of `Variable` in the environment.
+
 #include <optional>
 #include <string_view>
 
@@ -7,59 +21,56 @@ namespace datadog {
 namespace tracing {
 namespace environment {
 
-// Keep this sorted.  The values must correspond to offsets within
-// `variable_names`.
-// To ensure that the sorted `enum` names corresponding to the sorted
-// `variable_names`, each `enum` name must be equal to the corresponding
-// environment variable name.
+// To enforce correspondence between `enum Variable` and `variable_names`, the
+// preprocessor is used so that the DD_* symbols are listed exactly once.
+#define LIST_ENVIRONMENT_VARIABLES(MACRO) \
+  MACRO(DD_AGENT_HOST) \
+  MACRO(DD_ENV) \
+  MACRO(DD_PROPAGATION_STYLE_EXTRACT) \
+  MACRO(DD_PROPAGATION_STYLE_INJECT) \
+  MACRO(DD_SERVICE) \
+  MACRO(DD_SPAN_SAMPLING_RULES) \
+  MACRO(DD_SPAN_SAMPLING_RULES_FILE) \
+  MACRO(DD_TAGS) \
+  MACRO(DD_TRACE_AGENT_PORT) \
+  MACRO(DD_TRACE_AGENT_URL) \
+  MACRO(DD_TRACE_DEBUG) \
+  MACRO(DD_TRACE_ENABLED) \
+  MACRO(DD_TRACE_RATE_LIMIT) \
+  MACRO(DD_TRACE_REPORT_HOSTNAME) \
+  MACRO(DD_TRACE_SAMPLE_RATE) \
+  MACRO(DD_TRACE_SAMPLING_RULES) \
+  MACRO(DD_TRACE_STARTUP_LOGS) \
+  MACRO(DD_TRACE_TAGS_PROPAGATION_MAX_LENGTH) \
+  MACRO(DD_VERSION)
+
+#define WITH_COMMA(ARG) ARG,
+
 enum Variable {
-  DD_AGENT_HOST,
-  DD_ENV,
-  DD_PROPAGATION_STYLE_EXTRACT,
-  DD_PROPAGATION_STYLE_INJECT,
-  DD_SERVICE,
-  DD_SPAN_SAMPLING_RULES,
-  DD_SPAN_SAMPLING_RULES_FILE,
-  DD_TAGS,
-  DD_TRACE_AGENT_PORT,
-  DD_TRACE_AGENT_URL,
-  DD_TRACE_DEBUG,
-  DD_TRACE_ENABLED,
-  DD_TRACE_RATE_LIMIT,
-  DD_TRACE_REPORT_HOSTNAME,
-  DD_TRACE_SAMPLE_RATE,
-  DD_TRACE_SAMPLING_RULES,
-  DD_TRACE_STARTUP_LOGS,
-  DD_TRACE_TAGS_PROPAGATION_MAX_LENGTH,
-  DD_VERSION,
+  LIST_ENVIRONMENT_VARIABLES(WITH_COMMA)
 };
 
-// Keep this sorted.  Offsets into this array are indicated by `Variable`
-// values.
+// Quoting a macro arguments requires this two-step.
+#define QUOTED_IMPL(ARG) #ARG
+#define QUOTED(ARG) QUOTED_IMPL(ARG)
+
+#define QUOTED_WITH_COMMA(ARG) WITH_COMMA(QUOTED(ARG))
+
 inline const char *const variable_names[] = {
-    "DD_AGENT_HOST",
-    "DD_ENV",
-    "DD_PROPAGATION_STYLE_EXTRACT",
-    "DD_PROPAGATION_STYLE_INJECT",
-    "DD_SERVICE",
-    "DD_SPAN_SAMPLING_RULES",
-    "DD_SPAN_SAMPLING_RULES_FILE",
-    "DD_TAGS",
-    "DD_TRACE_AGENT_PORT",
-    "DD_TRACE_AGENT_URL",
-    "DD_TRACE_DEBUG",
-    "DD_TRACE_ENABLED",
-    "DD_TRACE_RATE_LIMIT",
-    "DD_TRACE_REPORT_HOSTNAME",
-    "DD_TRACE_SAMPLE_RATE",
-    "DD_TRACE_SAMPLING_RULES",
-    "DD_TRACE_STARTUP_LOGS",
-    "DD_TRACE_TAGS_PROPAGATION_MAX_LENGTH",
-    "DD_VERSION",
+  LIST_ENVIRONMENT_VARIABLES(QUOTED_WITH_COMMA)
 };
 
+#undef QUOTED_WITH_COMMA
+#undef QUOTED
+#undef QUOTED_IMPL
+#undef WITH_COMMA
+#undef LIST_ENVIRONMENT_VARIABLES
+
+// Return the name of the specified environment `variable`.
 std::string_view name(Variable variable);
 
+// Return the value of the specified environment `variable`, or return
+// `std::nullptr` if that variable is not set in the environment.
 std::optional<std::string_view> lookup(Variable variable);
 
 }  // namespace environment
