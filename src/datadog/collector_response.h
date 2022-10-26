8b8ab1195d82fa88046c067a062a2ed68828e259
David Goffredo <david.goffredo@datadoghq.com>
2022-10-26 16:06:18 -0400

    document collector_response.h

--- src/datadog/collector_response.h
+++ src/datadog/collector_response.h
@@ -1,5 +1,18 @@
 #pragma once
 
+// This component provides a `struct`, `CollectorResponse`, that contains
+// information that a `Collector` such as the Datadog Agent might deliver in
+// response to traces sent.
+//
+// It is really only filled out by the Datadog Agent, via `DatadogAgent` in
+// `datadog_agent.h`. It contains a mapping from keys to sample rates, where a
+// key identifies a service in a particular environment, and the sample rate is
+// the rate at which the trace sampler should keep traces produced by that
+// service in that environment.
+//
+// See `TraceSampler::handle_collector_response` in `trace_sampler.h` for more
+// information.
+
 #include <string>
 #include <string_view>
 #include <unordered_map>
