b03343aee37f4412ea04f2c1099d6f34d3972045
David Goffredo <david.goffredo@datadoghq.com>
2022-10-26 18:47:30 -0400

    document event_scheduler.h

--- src/datadog/event_scheduler.h
+++ src/datadog/event_scheduler.h
@@ -1,5 +1,14 @@
 #pragma once
 
+// This component provides an interface, `EventScheduler`, that allows a
+// specified function-like object to be invoked at regular intervals.
+//
+// `DatadogAgent` uses an `EventScheduler` to periodically send batches of
+// traces to the Datadog Agent.
+//
+// The default implementation is `ThreadedEventScheduler`.  See
+// `threaded_event_scheduler.h`.
+
 #include <chrono>
 #include <functional>
 
@@ -12,6 +21,10 @@ class EventScheduler {
  public:
   using Cancel = std::function<void()>;
 
+  // Invoke the specified `callback` repeatedly, with the specified `interval`
+  // elapsing between invocations.  The first invocation is after an initial
+  // `interval`.  Return a function-like object that can be invoked without
+  // arguments to prevent subsequent invocations of `callback`.
   virtual Cancel schedule_recurring_event(
       std::chrono::steady_clock::duration interval,
       std::function<void()> callback) = 0;
