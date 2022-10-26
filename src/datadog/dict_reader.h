37a4faad8fa458ee3d62b97b7feca1ca46f3571e
David Goffredo <david.goffredo@datadoghq.com>
2022-10-26 17:00:13 -0400

    document dict_reader.h

--- src/datadog/dict_reader.h
+++ src/datadog/dict_reader.h
@@ -1,5 +1,9 @@
 #pragma once
 
+// This component provides an interface, `DictReader`, that represents a
+// read-only key/value mapping of strings.  It's used when extracting trace
+// context from externalized formats: HTTP headers, gRPC metadata, etc.
+
 #include <functional>
 #include <optional>
 #include <string_view>
@@ -11,8 +15,12 @@ class DictReader {
  public:
   virtual ~DictReader() {}
 
+  // Return the value at the specified `key`, or return `std::nullopt` if there
+  // is no value at `key`.
   virtual std::optional<std::string_view> lookup(
       std::string_view key) const = 0;
+  
+  // Invoke the specified `visitor` once for each key/value pair in this object.
   virtual void visit(
       const std::function<void(std::string_view key, std::string_view value)>&
           visitor) const = 0;
