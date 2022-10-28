f45ed1ab857d986680bb1d326bad606edc9a4e88
David Goffredo <david.goffredo@datadoghq.com>
2022-10-28 11:25:12 -0400

    document null_collector.h

--- src/datadog/null_collector.h
+++ src/datadog/null_collector.h
@@ -1,5 +1,9 @@
 #pragma once
 
+// This component provides a `class`, `NullCollector`, that implements the
+// `Collector` interface in terms of a no-op. It's used by `Tracer` in lieu
+// of a `DatadogAgent` whenever `TracerConfig::report_traces` is `false`.
+
 #include "collector.h"
 
 namespace datadog {
