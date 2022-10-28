634e24f09cedf4a18aed6cf038e6180e1c9b4321
David Goffredo <david.goffredo@datadoghq.com>
2022-10-28 10:45:45 -0400

    document id_generator.h

--- src/datadog/id_generator.h
+++ src/datadog/id_generator.h
@@ -1,5 +1,16 @@
 #pragma once
 
+// This component provides facilities for generating sequences of IDs used as
+// span IDs and trace IDs.
+//
+// `IDGenerator` is an alias for `std::function<std::uint64_t()>`.
+//
+// `default_id_generator` is an `IDGenerator` that produces a thread-local
+// pseudo-random sequence of uniformly distributed 63-bit unsigned integers. The
+// sequence is randomly seeded one per thread and anytime the process forks.
+// The IDs are 63-bit (instead of 64-bit) to ease compatibility with peer
+// runtimes that don't have a native 64-bit unsigned numeric type.
+
 #include <cstdint>
 #include <functional>
 
