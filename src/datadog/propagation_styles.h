9d48effed3bfb59fec7210ea1c60c31c4e4d8f26
David Goffredo <david.goffredo@datadoghq.com>
2022-10-28 11:42:16 -0400

    document propagation_styles.h

--- src/datadog/propagation_styles.h
+++ src/datadog/propagation_styles.h
@@ -1,5 +1,10 @@
 #pragma once
 
+// This component provides a `struct`, `PropagationStyles`, that specifies which
+// trace context extraction or injection formats are to be used. `TracerConfig`
+// has one `PropagationStyles` for extraction and another for injection. See
+// `tracer_config.h`.
+
 namespace datadog {
 namespace tracing {
 
