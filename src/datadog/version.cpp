523f70482f0cc25420d9e026a9e7482259ecc812
David Goffredo <david.goffredo@datadoghq.com>
2022-11-08 14:47:46 -0500

    release v0.1.2

--- src/datadog/version.cpp
+++ src/datadog/version.cpp
@@ -3,8 +3,8 @@
 namespace datadog {
 namespace tracing {
 
-const char* const tracer_version = "v0.1.1";
-const char* const tracer_version_string = "[dd-trace-cpp version v0.1.1]";
+const char* const tracer_version = "v0.1.2";
+const char* const tracer_version_string = "[dd-trace-cpp version v0.1.2]";
 
 }  // namespace tracing
 }  // namespace datadog
