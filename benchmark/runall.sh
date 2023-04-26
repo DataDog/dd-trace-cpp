16a40f9230e013ed0c6e52df0fd50a531fc1837c
Dmytro Yurchenko <dmytro.yurchenko@datadoghq.com>
2023-04-26 16:54:52 +0200

    Basic CMakeLists.txt

--- benchmark/runall.sh
+++ benchmark/runall.sh
@@ -1,3 +1,9 @@
 #!/bin/bash
 
-echo "Do nothing yet"
+SCRIPT_DIR=$(cd "$(dirname "$(readlink "$0" || echo "$0")")" && pwd)
+cd $SCRIPT_DIR
+
+cmake .
+cmake --build . --config Release
+
+./gbench --benchmark_format=json > results.json
