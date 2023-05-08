337338db018f4722a117039a520ae3301295f40b
David Goffredo <david.goffredo@datadoghq.com>
2023-05-08 16:59:13 -0400

    benchmark based on the example/

--- benchmark/runall.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-SCRIPT_DIR=$(cd "$(dirname "$(readlink "$0" || echo "$0")")" && pwd)
-cd $SCRIPT_DIR
-
-cmake .
-cmake --build . --config Release
-
-./gbench --benchmark_repetitions=10 --benchmark_format=json > results.json
