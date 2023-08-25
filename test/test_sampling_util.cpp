05bfce0275884a755cd7e9cdd1a5188c773d9c10
David Goffredo <david.goffredo@datadoghq.com>
2023-08-25 17:13:43 -0400

    reduce indentation of clang-format exempt table

--- test/test_sampling_util.cpp
+++ test/test_sampling_util.cpp
@@ -18,31 +18,31 @@ TEST_CASE("probabilistic sampling") {
   };
 
   // clang-format off
-    auto test_case = GENERATE(values<TestCase>({
-        {__LINE__, "Test very small traceID", UINT64_C(1), *Rate::from(0.5), true},
-        {__LINE__, "Test very small traceID", UINT64_C(10), *Rate::from(0.5), false},
-        {__LINE__, "Test very small traceID", UINT64_C(100), *Rate::from(0.5), true},
-        {__LINE__, "Test very small traceID", UINT64_C(1000), *Rate::from(0.5), true},
-        {__LINE__, "Test random very large traceID", UINT64_C(18444899399302180860), *Rate::from(0.5), false},
-        {__LINE__, "Test random very large traceID", UINT64_C(18444899399302180861), *Rate::from(0.5), false},
-        {__LINE__, "Test random very large traceID", UINT64_C(18444899399302180862), *Rate::from(0.5), true},
-        {__LINE__, "Test random very large traceID", UINT64_C(18444899399302180863), *Rate::from(0.5), true},
-        {__LINE__, "Test the maximum traceID value 2**64-1", UINT64_C(18446744073709551615), *Rate::from(0.5), false},
-        {__LINE__, "Test 2**63+1", UINT64_C(9223372036854775809), *Rate::from(0.5), false},
-        {__LINE__, "Test 2**63-1", UINT64_C(9223372036854775807), *Rate::from(0.5), true},
-        {__LINE__, "Test 2**62+1", UINT64_C(4611686018427387905), *Rate::from(0.5), false},
-        {__LINE__, "Test 2**62-1", UINT64_C(4611686018427387903), *Rate::from(0.5), false},
-        {__LINE__, "10 random traceIDs", UINT64_C(646771306295669658), *Rate::from(0.5), true},
-        {__LINE__, "10 random traceIDs", UINT64_C(1882305164521835798), *Rate::from(0.5), true},
-        {__LINE__, "10 random traceIDs", UINT64_C(5198373796167680436), *Rate::from(0.5), false},
-        {__LINE__, "10 random traceIDs", UINT64_C(6272545487220484606), *Rate::from(0.5), true},
-        {__LINE__, "10 random traceIDs", UINT64_C(8696342848850656916), *Rate::from(0.5), true},
-        {__LINE__, "10 random traceIDs", UINT64_C(10197320802478874805), *Rate::from(0.5), true},
-        {__LINE__, "10 random traceIDs", UINT64_C(10350218024687037124), *Rate::from(0.5), true},
-        {__LINE__, "10 random traceIDs", UINT64_C(12078589664685934330), *Rate::from(0.5), false},
-        {__LINE__, "10 random traceIDs", UINT64_C(13794769880582338323), *Rate::from(0.5), true},
-        {__LINE__, "10 random traceIDs", UINT64_C(14629469446186818297), *Rate::from(0.5), false},
-    }));
+  auto test_case = GENERATE(values<TestCase>({
+    {__LINE__, "Test very small traceID", UINT64_C(1), *Rate::from(0.5), true},
+    {__LINE__, "Test very small traceID", UINT64_C(10), *Rate::from(0.5), false},
+    {__LINE__, "Test very small traceID", UINT64_C(100), *Rate::from(0.5), true},
+    {__LINE__, "Test very small traceID", UINT64_C(1000), *Rate::from(0.5), true},
+    {__LINE__, "Test random very large traceID", UINT64_C(18444899399302180860), *Rate::from(0.5), false},
+    {__LINE__, "Test random very large traceID", UINT64_C(18444899399302180861), *Rate::from(0.5), false},
+    {__LINE__, "Test random very large traceID", UINT64_C(18444899399302180862), *Rate::from(0.5), true},
+    {__LINE__, "Test random very large traceID", UINT64_C(18444899399302180863), *Rate::from(0.5), true},
+    {__LINE__, "Test the maximum traceID value 2**64-1", UINT64_C(18446744073709551615), *Rate::from(0.5), false},
+    {__LINE__, "Test 2**63+1", UINT64_C(9223372036854775809), *Rate::from(0.5), false},
+    {__LINE__, "Test 2**63-1", UINT64_C(9223372036854775807), *Rate::from(0.5), true},
+    {__LINE__, "Test 2**62+1", UINT64_C(4611686018427387905), *Rate::from(0.5), false},
+    {__LINE__, "Test 2**62-1", UINT64_C(4611686018427387903), *Rate::from(0.5), false},
+    {__LINE__, "10 random traceIDs", UINT64_C(646771306295669658), *Rate::from(0.5), true},
+    {__LINE__, "10 random traceIDs", UINT64_C(1882305164521835798), *Rate::from(0.5), true},
+    {__LINE__, "10 random traceIDs", UINT64_C(5198373796167680436), *Rate::from(0.5), false},
+    {__LINE__, "10 random traceIDs", UINT64_C(6272545487220484606), *Rate::from(0.5), true},
+    {__LINE__, "10 random traceIDs", UINT64_C(8696342848850656916), *Rate::from(0.5), true},
+    {__LINE__, "10 random traceIDs", UINT64_C(10197320802478874805), *Rate::from(0.5), true},
+    {__LINE__, "10 random traceIDs", UINT64_C(10350218024687037124), *Rate::from(0.5), true},
+    {__LINE__, "10 random traceIDs", UINT64_C(12078589664685934330), *Rate::from(0.5), false},
+    {__LINE__, "10 random traceIDs", UINT64_C(13794769880582338323), *Rate::from(0.5), true},
+    {__LINE__, "10 random traceIDs", UINT64_C(14629469446186818297), *Rate::from(0.5), false},
+  }));
   // clang-format on
 
   CAPTURE(test_case.name);
