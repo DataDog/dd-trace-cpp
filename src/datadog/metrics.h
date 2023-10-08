70ad77c4a8207245da4ddda0d064f3593be00233
Caleb Gilmour <caleb.gilmour@datadoghq.com>
2023-10-08 01:43:35 +0000

    Comments

--- src/datadog/metrics.h
+++ src/datadog/metrics.h
@@ -1,5 +1,11 @@
 #pragma once
 
+// This component provides an interface, `Metric`, and specific classes for
+// Counter and Gauge metrics. A metric has a name, type, and set of key:value
+// tags associated with it. Metrics can be general to APM or language-specific.
+// General metrics have `common` set to `true`, and language-specific metrics
+// have `common` set to `false`.
+
 #include <atomic>
 #include <string>
 #include <vector>
@@ -27,8 +33,8 @@ class Metric {
          const std::vector<std::string> tags, bool common);
 
  public:
-  // Accessors for name, type, tags, common and value are used when producing
-  // the JSON message for reporting metrics.
+  // Accessors for name, type, tags, common and capture_and_reset_value are used
+  // when producing the JSON message for reporting metrics.
   const std::string name();
   const std::string type();
   const std::vector<std::string> tags();
@@ -37,6 +43,8 @@ class Metric {
   uint64_t capture_and_reset_value();
 };
 
+// A count metric is used for measuring activity, and has methods for adding a
+// number of actions, or incrementing the current number of actions by 1.
 class CounterMetric : public Metric {
  public:
   CounterMetric(const std::string name, const std::vector<std::string> tags,
@@ -45,6 +53,9 @@ class CounterMetric : public Metric {
   void add(uint64_t amount);
 };
 
+// A gauge metric is used for measuring state, and mas methods to set the
+// current state, add or subtract from it, or increment/decrement the current
+// state by 1.
 class GaugeMetric : public Metric {
  public:
   GaugeMetric(const std::string name, const std::vector<std::string> tags,
