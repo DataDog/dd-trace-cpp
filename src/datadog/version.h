5c531434587196b9da86b992c39675df42e89b4d
David Goffredo <david.goffredo@datadoghq.com>
2023-11-17 17:51:36 -0500

    release candidate v0.1.12

--- src/datadog/version.h
+++ src/datadog/version.h
@@ -5,14 +5,14 @@
 namespace datadog {
 namespace tracing {
 
-// The release version at or before this code revision, e.g. "v0.1.1".
+// The release version at or before this code revision, e.g. "v0.1.12".
 // That is, this code is at least as recent as `tracer_version`, but may be
 // more recent.
 extern const char *const tracer_version;
 
 // A string literal that contains `tracer_version` but also is easier to `grep`
 // from the output of the `strings` command line utility, e.g. "[dd-trace-cpp
-// version v0.1.1]".
+// version v0.1.12]".
 extern const char *const tracer_version_string;
 
 }  // namespace tracing
