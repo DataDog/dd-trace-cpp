76182918a73bcaaa8737b0e8a33337b9cc3aa3f4
David Goffredo <david.goffredo@datadoghq.com>
2024-01-03 15:23:35 -0500

    add documentation TODOs

--- src/datadog/config_update.h
+++ src/datadog/config_update.h
@@ -1,10 +1,11 @@
+// TODO: Document
+
 #include "optional"
 #include "trace_sampler.h"
 
 namespace datadog {
 namespace tracing {
 
-// TODO: Document
 struct ConfigUpdate {
   Optional<TraceSamplerConfig> trace_sampler;
 };
--- src/datadog/remote_config.h
+++ src/datadog/remote_config.h
@@ -16,12 +16,14 @@ namespace datadog {
 namespace tracing {
 
 class RemoteConfigurationManager {
+  // TODO: document
   struct State {
     uint64_t targets_version = 1;
     std::string opaque_backend_state;
     Optional<std::string> error_message;
   };
 
+  // TODO: document
   struct Configuration {
     std::string id;
     std::string hash;
@@ -31,23 +33,30 @@ class RemoteConfigurationManager {
 
   TracerSignature tracer_signature_;
   ConfigManager& config_manager_;
+  // TODO: document
   std::string client_id_;
 
   State state_;
+  // TODO: document
   std::unordered_map<std::string, Configuration> applied_config_;
 
  public:
   RemoteConfigurationManager(const TracerSignature& tracer_signature,
                              ConfigManager& config_manager);
 
+  // TODO: document
   nlohmann::json make_request_payload();
 
+  // TODO: document
   void process_response(const nlohmann::json& json);
 
  private:
+  // TODO: document
   bool is_new_config(StringView config_path, const nlohmann::json& config_meta);
 
+  // TODO: document
   void apply_config(Configuration config);
+  // TODO: document
   void revert_config(Configuration config);
 };
 
