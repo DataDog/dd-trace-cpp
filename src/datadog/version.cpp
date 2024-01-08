f51971f267308ade28d249845214650916625900
roberto montero <roberto.montero@datadoghq.com>
2024-01-08 09:44:07 +0100

    debug cpp tracer

--- src/datadog/version.cpp
+++ src/datadog/version.cpp
@@ -3,7 +3,7 @@
 namespace datadog {
 namespace tracing {
 
-#define VERSION "v0.1.12"
+#define VERSION "v0.1.1299"
 
 const char* const tracer_version = VERSION;
 const char* const tracer_version_string = "[dd-trace-cpp version " VERSION "]";
