0de826789581029021c8ad05da161bc3a1bab55e
Damien Mehala <damien.mehala@datadoghq.com>
2024-03-04 17:25:54 +0100

    moar luajit

--- bindings/lua/ffi/ddtrace.lua
+++ bindings/lua/ffi/ddtrace.lua
@@ -1,16 +1,72 @@
 local ffi = require("ffi")
 
 ffi.cdef [[
-  int return_one_two_four();
-  void* make_tracer();
-  void* create_span(void*);
-  void span_gc(void*);
-  void tracer_gc(void*);
-  void set_span(void*, const char* const, const char* const);
+  typedef const char* (*ReaderFunc)(const char*);
+  typedef void (*WriterFunc)(const char*, const char*);
+
+  // Tracer
+  void* tracer_new();
+  void tracer_free(void*);
+  void* tracer_create_span(void*, const char*);
+  void* tracer_extract_or_create_span(void*, ReaderFunc);
+
+  // Span
+  void span_free(void*);
+  void span_set_tag(void*, const char*, const char*);
+  void span_inject(void*, WriterFunc);
+  void* span_create_child(void*, const char*);
+  void span_finish(void*);
 ]]
 
 local lib_ddtrace = ffi.load("ddtrace.so")
 
-local tracer = ffi.gc(lib_ddtrace.make_tracer(), lib_ddtrace.tracer_gc)
-local span = ffi.gc(lib_ddtrace.create_span(tracer), lib_ddtrace.span_gc)
-lib_ddtrace.set_span(span, "team", "apm-proxy")
+local function make_tracer()
+  return ffi.gc(lib_ddtrace.tracer_new(), lib_ddtrace.tracer_free)
+end
+
+local function create_span(tracer, name)
+  return ffi.gc(lib_ddtrace.tracer_create_span(tracer, name), lib_ddtrace.span_free)
+end
+
+local function set_tag(span, key, value)
+  lib_ddtrace.span_set_tag(span, key, value)
+end
+
+local function extract_or_create_span(tracer, reader_cb)
+  return ffi.gc(lib_ddtrace.tracer_extract_or_create_span(tracer, reader_cb), lib_ddtrace.span_free)
+end
+
+local function inject_span(span, writer_cb)
+  lib_ddtrace.span_inject(span, writer_cb)
+end
+
+local function create_child(span, name)
+  return ffi.gc(lib_ddtrace.span_create_child(span, name), lib_ddtrace.span_free)
+end
+
+local function finish_span(span)
+  lib_ddtrace.span_finish(span)
+end
+
+local tracer = make_tracer()
+local span = create_span(tracer, "root-span")
+set_tag(span, "team", "apm-proxy")
+set_tag(span, "user", "dmehala")
+
+local lua_reader = function(key)
+  print("[reader] key: " .. ffi.string(key))
+  -- return "a"
+  return nil
+end
+
+local lua_writer = function(key, value)
+  -- print("hello")
+  print("[writer] key: " .. ffi.string(key) .. ", value: " .. ffi.string(value))
+end
+
+local child = create_child(span, "child_span")
+finish_span(child)
+finish_span(span)
+
+-- local extracted_span = extract_or_create_span(tracer, lua_reader)
+-- inject_span(extracted_span, lua_writer)
--- bindings/lua/ffi/tracer.cpp
+++ bindings/lua/ffi/tracer.cpp
@@ -1,10 +1,47 @@
+#include "tracer.h"
+
+#include <datadog/dict_reader.h>
+#include <datadog/dict_writer.h>
 #include <datadog/span_config.h>
 #include <datadog/tracer.h>
+
+#include <iostream>
 
 namespace dd = datadog::tracing;
 
 extern "C" {
-void* make_tracer() {
+
+class LuaReader : public dd::DictReader {
+  ReaderFunc read_;
+
+ public:
+  LuaReader(ReaderFunc func) : read_(func) {}
+
+  dd::Optional<dd::StringView> lookup(dd::StringView key) const override {
+    if (auto value = read_(key.data())) {
+      return value;
+    }
+
+    return dd::nullopt;
+  }
+
+  void visit(const std::function<void(dd::StringView key,
+                                      dd::StringView value)>& /* visitor */)
+      const override{};
+};
+
+class LuaWriter : public dd::DictWriter {
+  WriterFunc write_;
+
+ public:
+  LuaWriter(WriterFunc func) : write_(func) {}
+  void set(dd::StringView key, dd::StringView value) override {
+    write_(key.data(), value.data());
+  }
+};
+
+// TRACER
+void* tracer_new() {
   dd::TracerConfig config;
   config.defaults.service = "luajit-dmehala";
 
@@ -12,30 +49,65 @@ void* make_tracer() {
   return new dd::Tracer{*validated_config};
 }
 
-void tracer_gc(void* p) {
+void tracer_free(void* p) {
   dd::Tracer* tracer = (dd::Tracer*)p;
   delete tracer;
 }
 
-void* create_span(void* p) {
+void* tracer_create_span(void* p, const char* name) {
   dd::Tracer* tracer = (dd::Tracer*)p;
 
   dd::SpanConfig opt;
-  opt.name = "root_span";
+  opt.name = name;
   void* buffer = new dd::Span(tracer->create_span(opt));
 
   return buffer;
 }
 
-void span_gc(void* p) {
+void* tracer_extract_or_create_span(void* p, ReaderFunc lua_reader) {
+  dd::Tracer* tracer = (dd::Tracer*)p;
+
+  LuaReader reader(lua_reader);
+  auto maybe_span = tracer->extract_or_create_span(reader);
+  if (auto error = maybe_span.if_error()) {
+    return nullptr;
+  }
+
+  void* buffer = new dd::Span(std::move(*maybe_span));
+  return buffer;
+}
+
+// SPAN
+void span_free(void* p) {
   dd::Span* span = (dd::Span*)p;
   delete span;
 }
 
-void set_span(void* p, const char* const key, const char* const value) {
+void span_set_tag(void* p, const char* const key, const char* value) {
   dd::Span* span = (dd::Span*)p;
   span->set_tag(key, value);
 }
 
-int return_one_two_four() { return 124; }
+void span_inject(void* p, WriterFunc lua_writer) {
+  dd::Span* span = (dd::Span*)p;
+
+  LuaWriter writer(lua_writer);
+  span->inject(writer);
+}
+
+void* span_create_child(void* p, const char* name) {
+  dd::Span* span = (dd::Span*)p;
+
+  dd::SpanConfig config;
+  config.name = name;
+
+  void* buffer = new dd::Span(span->create_child(config));
+
+  return buffer;
+}
+
+void span_finish(void* p) {
+  dd::Span* span = (dd::Span*)p;
+  span->set_end_time(std::chrono::steady_clock::now());
+}
 }
