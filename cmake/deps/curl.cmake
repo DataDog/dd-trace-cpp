449983c2ef32e43df5b1aa9f6053769d5da2b042
Damien Mehala <damien.mehala@datadoghq.com>
2024-06-16 22:32:12 +0200

    deps: bump libcurl to 8.3.0
    - Fix crashes on macOS Sonoma:
      https://github.com/curl/curl/issues/11893
    - Disable more curl features.
    - Debug build do not enable santiizer anymore.

--- cmake/deps/curl.cmake
+++ cmake/deps/curl.cmake
@@ -4,25 +4,32 @@ include(FetchContent)
 SET(BUILD_CURL_EXE OFF)
 SET(BUILD_SHARED_LIBS OFF)
 SET(BUILD_STATIC_LIBS ON)
+set(BUILD_LIBCURL_DOCS OFF)
+set(BUILD_MISC_DOCS OFF)
 
-# Disables all protocols except HTTP
+# Disable all protocols except HTTP
 SET(HTTP_ONLY ON)
+
+# Disable curl features
+SET(USE_ZLIB OFF)
+SET(USE_LIBIDN2 OFF)
 SET(CURL_ENABLE_SSL OFF)
 SET(CURL_BROTLI OFF)
 SET(CURL_ZSTD OFF)
 SET(CURL_ZLIB OFF)
-SET(USE_ZLIB OFF)
-SET(USE_LIBIDN2 OFF)
 SET(CURL_USE_LIBSSH2 OFF)
 SET(CURL_USE_LIBPSL OFF)
 SET(CURL_DISABLE_HSTS ON)
 set(CURL_CA_PATH "none")
 set(CURL_CA_PATH_SET FALSE)
+set(CURL_DISABLE_INSTALL ON)
+set(CURL_DISABLE_ALTSVC ON)
+set(CURL_DISABLE_SRP ON)
 
 FetchContent_Declare(
   curl
-  URL "https://github.com/curl/curl/releases/download/curl-7_85_0/curl-7.85.0.tar.gz"
-  URL_MD5 "4e9eb4f434e9be889e510f038754d3de"
+  URL "https://github.com/curl/curl/releases/download/curl-8_8_0/curl-8.8.0.tar.gz"
+  URL_MD5 "2300048f61e6196678281a8612a873ef"
 )
 
 FetchContent_MakeAvailable(curl)
