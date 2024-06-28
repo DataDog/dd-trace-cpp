DataDog/dd-trace-cpp

commit 440c28e4fd9b5ca725023a87c785023e5ff73ea8

enforce fixed length of 16 for _dd.p.tid (#42)
* enforce fixed length of 16 for _dd.p.tid

* integer parsing doesn't skip whitespace

* unfactor setting of _dd.propagation_error tag

* add a clarifying comment

* test parse_int and parse_uint64

test/test_tracer.cpp
@@ -1042,11 +1042,30 @@ TEST_CASE("128-bit trace IDs") {
     trace_id = span->trace_id();
   }
 
+  SECTION("are, for W3C, extracted preferentially from traceparent") {
+    auto tid = GENERATE(values<std::string>({"decade", "deadbeefdeadbeed"}));
+    std::unordered_map<std::string, std::string> headers;
+    headers["traceparent"] =
+        "00-deadbeefdeadbeefcafebabecafebabe-0000000000000001-01";
+    // The _dd.p.tid value below is either malformed or inconsistent with the
+    // trace ID in the traceparent.
+    // It will be ignored, and the resulting _dd.p.tid value will be consistent
+    // with the higher part of the trace ID in traceparent: "deadbeefdeadbeef".
+    headers["tracestate"] = "dd=t.tid:" + tid;
+    MockDictReader reader{headers};
+    const auto span = tracer.extract_span(reader);
+    CAPTURE(logger->entries);
+    REQUIRE(logger->error_count() == 0);
+    REQUIRE(span);
+    REQUIRE(hex(span->trace_id().high) == "deadbeefdeadbeef");
+    trace_id = span->trace_id();
+  }
+
   SECTION("are extracted from Datadog (_dd.p.tid)") {
     std::unordered_map<std::string, std::string> headers;
     headers["x-datadog-trace-id"] = "4";
     headers["x-datadog-parent-id"] = "42";
-    headers["x-datadog-tags"] = "_dd.p.tid=beef";
+    headers["x-datadog-tags"] = "_dd.p.tid=000000000000beef";
     MockDictReader reader{headers};
     const auto span = tracer.extract_span(reader);
     CAPTURE(logger->entries);
@@ -1094,7 +1113,9 @@ TEST_CASE(
 
   auto test_case = GENERATE(values<TestCase>(
       {{__LINE__, "invalid _dd.p.tid", "noodle", "malformed_tid "},
-       {__LINE__, "_dd.p.tid inconsistent with trace ID", "adfeed",
+       {__LINE__, "short _dd.p.tid", "beef", "malformed_tid "},
+       {__LINE__, "long _dd.p.tid", "000000000000000000beef", "malformed_tid "},
+       {__LINE__, "_dd.p.tid inconsistent with trace ID", "0000000000adfeed",
         "inconsistent_tid "}}));
 
   CAPTURE(test_case.line);

src/datadog/parse_util.cpp
@@ -17,7 +17,6 @@ namespace {
 template <typename Integer>
 Expected<Integer> parse_integer(StringView input, int base, StringView kind) {
   Integer value;
-  input = strip(input);
   const auto status = std::from_chars(input.begin(), input.end(), value, base);
   if (status.ec == std::errc::invalid_argument) {
     std::string message;

src/datadog/tracer.cpp
@@ -27,6 +27,22 @@ namespace datadog {
 namespace tracing {
 namespace {
 
+// Parse the high 64 bits of a trace ID from the specified `value`. If `value`
+// is correctly formatted, then return the resulting bits. If `value` is
+// incorrectly formatted then return `nullopt`.
+Optional<std::uint64_t> parse_trace_id_high(const std::string& value) {
+  if (value.size() != 16) {
+    return nullopt;
+  }
+
+  auto high = parse_uint64(value, 16);
+  if (high) {
+    return *high;
+  }
+
+  return nullopt;
+}
+
 // Decode the specified `trace_tags` and integrate them into the specified
 // `result`. If an error occurs, add a `tags::internal::propagation_error` tag
 // to the specified `span_tags` and log a diagnostic using the specified
@@ -48,12 +64,8 @@ void handle_trace_tags(StringView trace_tags, ExtractedData& result,
 
     if (key == tags::internal::trace_id_high) {
       // _dd.p.tid contains the high 64 bits of the trace ID.
-      auto high = parse_uint64(value, 16);
-      if (auto* error = high.if_error()) {
-        logger.log_error(
-            error->with_prefix("Unable to parse high bits of the trace ID in "
-                               "Datadog style from the "
-                               "\"_dd.p.tid\" trace tag: "));
+      const Optional<std::uint64_t> high = parse_trace_id_high(value);
+      if (!high) {
         span_tags[tags::internal::propagation_error] = "malformed_tid " + value;
         continue;
       }
@@ -274,7 +286,7 @@ Span Tracer::create_span(const SpanConfig& config) {
   span_data->trace_id = generator_->trace_id(span_data->start);
   if (span_data->trace_id.high) {
     trace_tags.emplace_back(tags::internal::trace_id_high,
-                            hex(span_data->trace_id.high));
+                            hex_padded(span_data->trace_id.high));
   }
   span_data->span_id = span_data->trace_id.low;
   span_data->parent_id = 0;
@@ -401,21 +413,27 @@ Expected<Span> Tracer::extract_span(const DictReader& reader,
     //
     // First, though, if the `trace_id_high` tag is already set and has a bogus
     // value or a value inconsistent with the trace ID, tag an error.
-    const std::string hex_high = hex(span_data->trace_id.high);
+    const auto hex_high = hex_padded(span_data->trace_id.high);
     const auto extant = std::find_if(
         trace_tags.begin(), trace_tags.end(), [&](const auto& pair) {
           return pair.first == tags::internal::trace_id_high;
         });
     if (extant == trace_tags.end()) {
       trace_tags.emplace_back(tags::internal::trace_id_high, hex_high);
-    } else if (!parse_uint64(extant->second, 16)) {
-      span_data->tags[tags::internal::propagation_error] =
-          "malformed_tid " + extant->second;
-      extant->second = hex_high;
-    } else if (extant->second != hex_high) {
-      span_data->tags[tags::internal::propagation_error] =
-          "inconsistent_tid " + extant->second;
-      extant->second = hex_high;
+    } else {
+      // There is already a `trace_id_high` tag. `hex_high` is its proper value.
+      // Check if the extant value is malformed or different from `hex_high`. In
+      // either case, tag an error and overwrite the tag with `hex_high`.
+      const Optional<std::uint64_t> high = parse_trace_id_high(extant->second);
+      if (!high) {
+        span_data->tags[tags::internal::propagation_error] =
+            "malformed_tid " + extant->second;
+        extant->second = hex_high;
+      } else if (*high != span_data->trace_id.high) {
+        span_data->tags[tags::internal::propagation_error] =
+            "inconsistent_tid " + extant->second;
+        extant->second = hex_high;
+      }
     }
   }
 

test/CMakeLists.txt
@@ -23,6 +23,7 @@ add_executable(tests
     test_glob.cpp
     test_limiter.cpp
     test_msgpack.cpp
+    test_parse_util.cpp
     test_smoke.cpp
     test_span.cpp
     test_span_sampler.cpp

test/test_parse_util.cpp
@@ -1,0 +1,135 @@
+#include <datadog/error.h>
+#include <datadog/parse_util.h>
+
+#include <cassert>
+#include <cstdint>
+#include <limits>
+#include <string>
+#include <variant>
+
+#include "test.h"
+
+using namespace datadog::tracing;
+
+TEST_CASE("parse_int") {
+  struct TestCase {
+    int line;
+    std::string name;
+    std::string argument;
+    int base;
+    std::variant<int, Error::Code> expected;
+  };
+
+  // clang-format off
+  auto test_case = GENERATE(values<TestCase>({
+      {__LINE__, "zero (dec)", "0", 10, 0},
+      {__LINE__, "zeros (dec)", "000", 10, 0},
+      {__LINE__, "zero (hex)", "0", 16, 0},
+      {__LINE__, "zeros (hex)", "000", 16, 0},
+      {__LINE__, "leading whitespace (dec 1)", " 42", 10, Error::INVALID_INTEGER},
+      {__LINE__, "leading whitespace (dec 2)", "\t42", 10, Error::INVALID_INTEGER},
+      {__LINE__, "leading whitespace (dec 3)", "\n42", 10, Error::INVALID_INTEGER},
+      {__LINE__, "trailing whitespace (dec 1)", "42 ", 10, Error::INVALID_INTEGER},
+      {__LINE__, "trailing whitespace (dec 2)", "42\t", 10, Error::INVALID_INTEGER},
+      {__LINE__, "trailing whitespace (dec 3)", "42\n", 10, Error::INVALID_INTEGER},
+      {__LINE__, "leading whitespace (hex 1)", " 42", 16, Error::INVALID_INTEGER},
+      {__LINE__, "leading whitespace (hex 2)", "\t42", 16, Error::INVALID_INTEGER},
+      {__LINE__, "leading whitespace (hex 3)", "\n42", 16, Error::INVALID_INTEGER},
+      {__LINE__, "trailing whitespace (hex 1)", "42 ", 16, Error::INVALID_INTEGER},
+      {__LINE__, "trailing whitespace (hex 2)", "42\t", 16, Error::INVALID_INTEGER},
+      {__LINE__, "trailing whitespace (hex 3)", "42\n", 16, Error::INVALID_INTEGER},
+      {__LINE__, "no hex prefix", "0xbeef", 16, Error::INVALID_INTEGER},
+      {__LINE__, "dec rejects hex", "42beef", 10, Error::INVALID_INTEGER},
+      {__LINE__, "hex accepts hex", "42beef", 16, 0x42beef},
+      {__LINE__, "no trailing nonsense (dec)", "42xyz", 10, Error::INVALID_INTEGER},
+      {__LINE__, "no trailing nonsense (hex)", "42xyz", 16, Error::INVALID_INTEGER},
+      {__LINE__, "no leading nonsense (dec)", "xyz42", 10, Error::INVALID_INTEGER},
+      {__LINE__, "no leading nonsense (hex)", "xyz42", 16, Error::INVALID_INTEGER},
+      {__LINE__, "overflow", std::to_string(std::numeric_limits<int>::max()) + "0", 10, Error::OUT_OF_RANGE_INTEGER},
+      {__LINE__, "negative (dec)", "-10", 10, -10},
+      {__LINE__, "negative (hex)", "-a", 16, -10},
+      {__LINE__, "lower case", "a", 16, 10},
+      {__LINE__, "upper case", "A", 16, 10},
+      {__LINE__, "underflow", std::to_string(std::numeric_limits<int>::min()) + "0", 10, Error::OUT_OF_RANGE_INTEGER},
+  }));
+  // clang-format on
+
+  CAPTURE(test_case.line);
+  CAPTURE(test_case.name);
+  CAPTURE(test_case.argument);
+  CAPTURE(test_case.base);
+
+  const auto result = parse_int(test_case.argument, test_case.base);
+  if (std::holds_alternative<int>(test_case.expected)) {
+    const int& expected = std::get<int>(test_case.expected);
+    REQUIRE(result);
+    REQUIRE(*result == expected);
+  } else {
+    assert(std::holds_alternative<Error::Code>(test_case.expected));
+    const Error::Code& expected = std::get<Error::Code>(test_case.expected);
+    REQUIRE(!result);
+    REQUIRE(result.error().code == expected);
+  }
+}
+
+// This test case is similar to the one above, except that negative numbers are
+// not supported, and the underflow and overflow values are different.
+TEST_CASE("parse_uint64") {
+  struct TestCase {
+    int line;
+    std::string name;
+    std::string argument;
+    int base;
+    std::variant<std::uint64_t, Error::Code> expected;
+  };
+
+  // clang-format off
+  auto test_case = GENERATE(values<TestCase>({
+      {__LINE__, "zero (dec)", "0", 10, UINT64_C(0)},
+      {__LINE__, "zeros (dec)", "000", 10, UINT64_C(0)},
+      {__LINE__, "zero (hex)", "0", 16, UINT64_C(0)},
+      {__LINE__, "zeros (hex)", "000", 16, UINT64_C(0)},
+      {__LINE__, "leading whitespace (dec 1)", " 42", 10, Error::INVALID_INTEGER},
+      {__LINE__, "leading whitespace (dec 2)", "\t42", 10, Error::INVALID_INTEGER},
+      {__LINE__, "leading whitespace (dec 3)", "\n42", 10, Error::INVALID_INTEGER},
+      {__LINE__, "trailing whitespace (dec 1)", "42 ", 10, Error::INVALID_INTEGER},
+      {__LINE__, "trailing whitespace (dec 2)", "42\t", 10, Error::INVALID_INTEGER},
+      {__LINE__, "trailing whitespace (dec 3)", "42\n", 10, Error::INVALID_INTEGER},
+      {__LINE__, "leading whitespace (hex 1)", " 42", 16, Error::INVALID_INTEGER},
+      {__LINE__, "leading whitespace (hex 2)", "\t42", 16, Error::INVALID_INTEGER},
+      {__LINE__, "leading whitespace (hex 3)", "\n42", 16, Error::INVALID_INTEGER},
+      {__LINE__, "trailing whitespace (hex 1)", "42 ", 16, Error::INVALID_INTEGER},
+      {__LINE__, "trailing whitespace (hex 2)", "42\t", 16, Error::INVALID_INTEGER},
+      {__LINE__, "trailing whitespace (hex 3)", "42\n", 16, Error::INVALID_INTEGER},
+      {__LINE__, "no hex prefix", "0xbeef", 16, Error::INVALID_INTEGER},
+      {__LINE__, "dec rejects hex", "42beef", 10, Error::INVALID_INTEGER},
+      {__LINE__, "hex accepts hex", "42beef", 16, UINT64_C(0x42beef)},
+      {__LINE__, "no trailing nonsense (dec)", "42xyz", 10, Error::INVALID_INTEGER},
+      {__LINE__, "no trailing nonsense (hex)", "42xyz", 16, Error::INVALID_INTEGER},
+      {__LINE__, "no leading nonsense (dec)", "xyz42", 10, Error::INVALID_INTEGER},
+      {__LINE__, "no leading nonsense (hex)", "xyz42", 16, Error::INVALID_INTEGER},
+      {__LINE__, "overflow", std::to_string(std::numeric_limits<std::uint64_t>::max()) + "0", 10, Error::OUT_OF_RANGE_INTEGER},
+      {__LINE__, "negative (dec)", "-10", 10, Error::INVALID_INTEGER},
+      {__LINE__, "negative (hex)", "-a", 16, Error::INVALID_INTEGER},
+      {__LINE__, "lower case", "a", 16, UINT64_C(10)},
+      {__LINE__, "upper case", "A", 16, UINT64_C(10)},
+  }));
+  // clang-format on
+
+  CAPTURE(test_case.line);
+  CAPTURE(test_case.name);
+  CAPTURE(test_case.argument);
+  CAPTURE(test_case.base);
+
+  const auto result = parse_uint64(test_case.argument, test_case.base);
+  if (std::holds_alternative<std::uint64_t>(test_case.expected)) {
+    const std::uint64_t& expected = std::get<std::uint64_t>(test_case.expected);
+    REQUIRE(result);
+    REQUIRE(*result == expected);
+  } else {
+    assert(std::holds_alternative<Error::Code>(test_case.expected));
+    const Error::Code& expected = std::get<Error::Code>(test_case.expected);
+    REQUIRE(!result);
+    REQUIRE(result.error().code == expected);
+  }
+}

commit deb1f8a74f82b0dcc85ecaa85e17ae4aab0de3f3

fix: trim inspected headers
Fixed a regression introduced in #42 preventing HTTP headers with
leading spaces to be parsed.

src/datadog/extraction_util.cpp
@@ -71,7 +71,7 @@ Expected<Optional<std::uint64_t>> extract_id_header(const DictReader& headers,
   if (!found) {
     return result;
   }
-  auto parsed_id = parse_uint64(*found, base);
+  auto parsed_id = parse_uint64(trim(*found), base);
   if (auto* error = parsed_id.if_error()) {
     std::string prefix;
     prefix += "Could not extract ";
@@ -127,7 +127,7 @@ Expected<ExtractedData> extract_datadog(
   result.parent_id = *parent_id;
 
   if (auto found = headers.lookup("x-datadog-sampling-priority")) {
-    auto sampling_priority = parse_int(*found, 10);
+    auto sampling_priority = parse_int(trim(*found), 10);
     if (auto* error = sampling_priority.if_error()) {
       std::string prefix;
       prefix +=
@@ -166,7 +166,7 @@ Expected<ExtractedData> extract_b3(
   result.style = PropagationStyle::B3;
 
   if (auto found = headers.lookup("x-b3-traceid")) {
-    auto parsed = TraceID::parse_hex(*found);
+    auto parsed = TraceID::parse_hex(trim(*found));
     if (auto* error = parsed.if_error()) {
       std::string prefix = "Could not extract B3-style trace ID from \"";
       append(prefix, *found);
@@ -185,7 +185,7 @@ Expected<ExtractedData> extract_b3(
 
   const StringView sampling_priority_header = "x-b3-sampled";
   if (auto found = headers.lookup(sampling_priority_header)) {
-    auto sampling_priority = parse_int(*found, 10);
+    auto sampling_priority = parse_int(trim(*found), 10);
     if (auto* error = sampling_priority.if_error()) {
       std::string prefix;
       prefix += "Could not extract B3-style sampling priority from ";

test/test_tracer.cpp
@@ -446,6 +446,15 @@ TEST_CASE("span extraction") {
          456,
          2},
         {__LINE__,
+         "datadog style with leading and trailing spaces",
+         {PropagationStyle::DATADOG},
+         {{"x-datadog-trace-id", "   123  "},
+          {"x-datadog-parent-id", " 456  "},
+          {"x-datadog-sampling-priority", "    2 "}},
+         TraceID(123),
+         456,
+         2},
+        {__LINE__,
          "datadog style without sampling priority",
          {PropagationStyle::DATADOG},
          {{"x-datadog-trace-id", "123"}, {"x-datadog-parent-id", "456"}},
@@ -465,6 +474,15 @@ TEST_CASE("span extraction") {
          {{"x-b3-traceid", "abc"},
           {"x-b3-spanid", "def"},
           {"x-b3-sampled", "0"}},
+         TraceID(0xabc),
+         0xdef,
+         0},
+        {__LINE__,
+         "B3 style with leading and trailing spaces",
+         {PropagationStyle::B3},
+         {{"x-b3-traceid", "   abc   "},
+          {"x-b3-spanid", " def  "},
+          {"x-b3-sampled", "     0  "}},
          TraceID(0xabc),
          0xdef,
          0},
@@ -593,6 +611,13 @@ TEST_CASE("span extraction") {
         // https://www.w3.org/TR/trace-context/#examples-of-http-traceparent-headers
         {__LINE__, "valid: w3.org example 1",
          "00-4bf92f3577b34da6a3ce929d0e0e4736-00f067aa0ba902b7-01", // traceparent
+         nullopt,
+         *TraceID::parse_hex("4bf92f3577b34da6a3ce929d0e0e4736"), // expected_trace_id
+         67667974448284343ULL, // expected_parent_id
+         1}, // expected_sampling_priority
+
+        {__LINE__, "valid: w3.org example 1 with leading and trailing spaces",
+         "   00-4bf92f3577b34da6a3ce929d0e0e4736-00f067aa0ba902b7-01   ", // traceparent
          nullopt,
          *TraceID::parse_hex("4bf92f3577b34da6a3ce929d0e0e4736"), // expected_trace_id
          67667974448284343ULL, // expected_parent_id