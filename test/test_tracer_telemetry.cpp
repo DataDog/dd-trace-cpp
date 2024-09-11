DataDog/dd-trace-cpp

commit f3958f3a21f123e16ed042a00ea30e911514ca9f

refactor: Remote Configuration (#130)
Refactor the Remote Configuration (RC) as a standalone module, enabling
its use independently of the tracer. Additionally, ensure that external
listeners can register and receive configuration updates when RC is
embedded in the tracer.

Changes:
  - Move APM_TRACING updates handling to the configuration manager.
  - Expose the `remote_configuration_listeners` field in datadog agent config
    to allow registration of external listeners.
  - Improve the robustness of config path parsing and ensure an error is reported
    if parsing fails.
  - Add `to_upper` utility function.
  - Increase test coverage for RC and the new configuration manager
    behaviour.

src/datadog/tracer_telemetry.cpp
@@ -230,6 +230,13 @@ void TracerTelemetry::capture_metrics() {
   }
 }
 
+void TracerTelemetry::capture_configuration_change(
+    const std::vector<ConfigMetadata>& new_configuration) {
+  configuration_snapshot_.insert(configuration_snapshot_.begin(),
+                                 new_configuration.begin(),
+                                 new_configuration.end());
+}
+
 std::string TracerTelemetry::heartbeat_and_telemetry() {
   auto batch_payloads = nlohmann::json::array();
 
@@ -281,6 +288,7 @@ std::string TracerTelemetry::heartbeat_and_telemetry() {
   auto telemetry_body = generate_telemetry_body("message-batch");
   telemetry_body["payload"] = batch_payloads;
   auto message_batch_payload = telemetry_body.dump();
+
   return message_batch_payload;
 }
 
@@ -335,22 +343,26 @@ std::string TracerTelemetry::app_closing() {
   auto telemetry_body = generate_telemetry_body("message-batch");
   telemetry_body["payload"] = batch_payloads;
   auto message_batch_payload = telemetry_body.dump();
+
   return message_batch_payload;
 }
 
-std::string TracerTelemetry::configuration_change(
-    const std::vector<ConfigMetadata>& new_configuration) {
+std::string TracerTelemetry::configuration_change() {
   auto configuration_json = nlohmann::json::array();
-  for (const auto& config_metadata : new_configuration) {
+  for (const auto& config_metadata : configuration_snapshot_) {
     // if (config_metadata.value.empty()) continue;
     configuration_json.emplace_back(
         generate_configuration_field(config_metadata));
   }
 
-  auto configuration_change =
-      generate_telemetry_body("app-client-configuration-change");
-  configuration_change["payload"] =
-      nlohmann::json{{"configuration", configuration_json}};
+  // clang-format off
+  auto configuration_change = nlohmann::json({
+    {"request_type", "app-client-configuration-change"},
+    {"payload", nlohmann::json{
+      {"configuration", configuration_json}
+    }}
+  });
+  // clang-format on
 
   return configuration_change.dump();
 }

test/test_tracer_telemetry.cpp
@@ -95,7 +95,7 @@ TEST_CASE("Tracer telemetry", "[telemetry]") {
       SECTION("generates a configuration change event") {
         SECTION("empty configuration generate a valid payload") {
           auto config_change_message = nlohmann::json::parse(
-              tracer_telemetry.configuration_change({}), nullptr, false);
+              tracer_telemetry.configuration_change(), nullptr, false);
           REQUIRE(config_change_message.is_discarded() == false);
 
           CHECK(config_change_message["request_type"] ==
@@ -111,9 +111,9 @@ TEST_CASE("Tracer telemetry", "[telemetry]") {
               {ConfigName::REPORT_TRACES, "", ConfigMetadata::Origin::DEFAULT,
                Error{Error::Code::OTHER, "empty field"}}};
 
+          tracer_telemetry.capture_configuration_change(new_config);
           auto config_change_message = nlohmann::json::parse(
-              tracer_telemetry.configuration_change(new_config), nullptr,
-              false);
+              tracer_telemetry.configuration_change(), nullptr, false);
           REQUIRE(config_change_message.is_discarded() == false);
 
           CHECK(config_change_message["request_type"] ==

BUILD.bazel
@@ -27,7 +27,7 @@ cc_library(
     "src/datadog/propagation_style.cpp",
     "src/datadog/random.cpp",
     "src/datadog/rate.cpp",
-    "src/datadog/remote_config.cpp",
+    "src/datadog/remote_config/remote_config.cpp",
     "src/datadog/runtime_id.cpp",
     "src/datadog/span.cpp",
     "src/datadog/span_data.cpp",
@@ -55,7 +55,6 @@ cc_library(
     "src/datadog/config.h",
     "src/datadog/clock.h",
     "src/datadog/config_manager.h",
-    "src/datadog/config_update.h",
     "src/datadog/collector.h",
     "src/datadog/collector_response.h",
 #     "src/datadog/curl.h", no libcurl
@@ -88,7 +87,10 @@ cc_library(
     "src/datadog/propagation_style.h",
     "src/datadog/random.h",
     "src/datadog/rate.h",
-    "src/datadog/remote_config.h",
+    "src/datadog/remote_config/capability.h",
+    "src/datadog/remote_config/listener.h",
+    "src/datadog/remote_config/product.h",
+    "src/datadog/remote_config/remote_config.h",
     "src/datadog/runtime_id.h",
     "src/datadog/sampling_decision.h",
     "src/datadog/sampling_mechanism.h",
@@ -118,6 +120,7 @@ cc_library(
     "src/datadog/w3c_propagation.h",
     ],
     strip_include_prefix = "src/",
+    copts = ["-Isrc/datadog"],
     visibility = ["//visibility:public"],
     deps = [
         "@com_google_absl//absl/strings",

CMakeLists.txt
@@ -103,7 +103,7 @@ target_sources(dd_trace_cpp-objects
     src/datadog/propagation_style.cpp
     src/datadog/random.cpp
     src/datadog/rate.cpp
-    src/datadog/remote_config.cpp
+    src/datadog/remote_config/remote_config.cpp
     src/datadog/runtime_id.cpp
     src/datadog/span.cpp
     src/datadog/span_data.cpp
@@ -137,7 +137,6 @@ target_sources(dd_trace_cpp-objects PUBLIC
   src/datadog/cerr_logger.h
   src/datadog/clock.h
   src/datadog/config_manager.h
-  src/datadog/config_update.h
   src/datadog/collector.h
   src/datadog/collector_response.h
   # src/datadog/curl.h except for curl.h
@@ -170,7 +169,9 @@ target_sources(dd_trace_cpp-objects PUBLIC
   src/datadog/propagation_style.h
   src/datadog/random.h
   src/datadog/rate.h
-  src/datadog/remote_config.h
+  src/datadog/remote_config/remote_config.h
+  src/datadog/remote_config/capability.h
+  src/datadog/remote_config/listener.h
   src/datadog/runtime_id.h
   src/datadog/sampling_decision.h
   src/datadog/sampling_mechanism.h
@@ -204,11 +205,10 @@ target_sources(dd_trace_cpp-objects PUBLIC
 # or installing the library.
 target_include_directories(dd_trace_cpp-objects
   PUBLIC
-    $<INSTALL_INTERFACE:src>
+    $<INSTALL_INTERFACE:src/datadog>
     $<BUILD_INTERFACE:${CMAKE_CURRENT_SOURCE_DIR}/src>
+    $<BUILD_INTERFACE:${CMAKE_CURRENT_SOURCE_DIR}/src/datadog>
 )
-
-add_dependencies(dd_trace_cpp-objects Threads::Threads)
 
 target_link_libraries(dd_trace_cpp-objects
   PUBLIC

src/datadog/config_manager.cpp
@@ -77,16 +77,71 @@ Expected<Rules> parse_trace_sampling_rules(const nlohmann::json& json_rules) {
   return parsed_rules;
 }
 
+ConfigManager::Update parse_dynamic_config(const nlohmann::json& j) {
+  ConfigManager::Update config_update;
+
+  if (auto sampling_rate_it = j.find("tracing_sampling_rate");
+      sampling_rate_it != j.cend() && sampling_rate_it->is_number()) {
+    config_update.trace_sampling_rate = sampling_rate_it->get<double>();
+  }
+
+  if (auto tags_it = j.find("tracing_tags");
+      tags_it != j.cend() && tags_it->is_array()) {
+    config_update.tags = tags_it->get<std::vector<StringView>>();
+  }
+
+  if (auto tracing_enabled_it = j.find("tracing_enabled");
+      tracing_enabled_it != j.cend() && tracing_enabled_it->is_boolean()) {
+    config_update.report_traces = tracing_enabled_it->get<bool>();
+  }
+
+  if (auto tracing_sampling_rules_it = j.find("tracing_sampling_rules");
+      tracing_sampling_rules_it != j.cend() &&
+      tracing_sampling_rules_it->is_array()) {
+    config_update.trace_sampling_rules = &(*tracing_sampling_rules_it);
+  }
+
+  return config_update;
+}
+
 }  // namespace
 
-ConfigManager::ConfigManager(const FinalizedTracerConfig& config)
+namespace rc = datadog::remote_config;
+
+ConfigManager::ConfigManager(const FinalizedTracerConfig& config,
+                             const std::shared_ptr<TracerTelemetry>& telemetry)
     : clock_(config.clock),
       default_metadata_(config.metadata),
       trace_sampler_(
           std::make_shared<TraceSampler>(config.trace_sampler, clock_)),
       rules_(config.trace_sampler.rules),
       span_defaults_(std::make_shared<SpanDefaults>(config.defaults)),
-      report_traces_(config.report_traces) {}
+      report_traces_(config.report_traces),
+      telemetry_(telemetry) {}
+
+rc::Products ConfigManager::get_products() { return rc::product::APM_TRACING; }
+
+rc::Capabilities ConfigManager::get_capabilities() {
+  using namespace rc::capability;
+  return APM_TRACING_SAMPLE_RATE | APM_TRACING_TAGS | APM_TRACING_ENABLED |
+         APM_TRACING_SAMPLE_RULES;
+}
+
+Optional<std::string> ConfigManager::on_update(const Configuration& config) {
+  const auto config_json = nlohmann::json::parse(config.content);
+  auto config_update = parse_dynamic_config(config_json.at("lib_config"));
+
+  auto config_metadata = apply_update(config_update);
+  telemetry_->capture_configuration_change(config_metadata);
+
+  // TODO:
+  return nullopt;
+}
+
+void ConfigManager::on_revert(const Configuration&) {
+  auto config_metadata = apply_update({});
+  telemetry_->capture_configuration_change(config_metadata);
+}
 
 std::shared_ptr<TraceSampler> ConfigManager::trace_sampler() {
   std::lock_guard<std::mutex> lock(mutex_);
@@ -103,7 +158,8 @@ bool ConfigManager::report_traces() {
   return report_traces_.value();
 }
 
-std::vector<ConfigMetadata> ConfigManager::update(const ConfigUpdate& conf) {
+std::vector<ConfigMetadata> ConfigManager::apply_update(
+    const ConfigManager::Update& conf) {
   std::vector<ConfigMetadata> metadata;
 
   std::lock_guard<std::mutex> lock(mutex_);
@@ -209,8 +265,6 @@ void ConfigManager::reset_config(ConfigName name, T& conf,
   conf.reset();
   metadata.emplace_back(default_metadata_[name]);
 }
-
-std::vector<ConfigMetadata> ConfigManager::reset() { return update({}); }
 
 nlohmann::json ConfigManager::config_json() const {
   std::lock_guard<std::mutex> lock(mutex_);

src/datadog/config_manager.h
@@ -8,16 +8,31 @@
 #include <mutex>
 
 #include "clock.h"
-#include "config_update.h"
 #include "json.hpp"
 #include "optional.h"
+#include "remote_config/listener.h"
 #include "span_defaults.h"
 #include "tracer_config.h"
+#include "tracer_telemetry.h"
 
 namespace datadog {
 namespace tracing {
 
-class ConfigManager {
+class ConfigManager : public remote_config::Listener {
+ public:
+  // The `Update` struct serves as a container for configuration that can
+  // exclusively be changed remotely.
+  //
+  // Configurations can be `nullopt` to signal the absence of a value from the
+  // remote configuration value.
+  struct Update {
+    Optional<bool> report_traces;
+    Optional<double> trace_sampling_rate;
+    Optional<std::vector<StringView>> tags;
+    const nlohmann::json* trace_sampling_rules = nullptr;
+  };
+
+ private:
   // A class template for managing dynamic configuration values.
   //
   // This class allows storing and managing dynamic configuration values. It
@@ -60,13 +75,25 @@ class ConfigManager {
   DynamicConfig<std::shared_ptr<const SpanDefaults>> span_defaults_;
   DynamicConfig<bool> report_traces_;
 
+  std::shared_ptr<TracerTelemetry> telemetry_;
+
  private:
   template <typename T>
   void reset_config(ConfigName name, T& conf,
                     std::vector<ConfigMetadata>& metadata);
 
  public:
-  ConfigManager(const FinalizedTracerConfig& config);
+  ConfigManager(const FinalizedTracerConfig& config,
+                const std::shared_ptr<TracerTelemetry>& telemetry);
+  ~ConfigManager() override{};
+
+  remote_config::Products get_products() override;
+  remote_config::Capabilities get_capabilities() override;
+
+  Optional<std::string> on_update(
+      const Listener::Configuration& config) override;
+  void on_revert(const Listener::Configuration& config) override;
+  void on_post_process() override{};
 
   // Return the `TraceSampler` consistent with the most recent configuration.
   std::shared_ptr<TraceSampler> trace_sampler();
@@ -77,16 +104,11 @@ class ConfigManager {
   // Return whether traces should be sent to the collector.
   bool report_traces();
 
-  // Apply the specified `conf` update.
-  std::vector<ConfigMetadata> update(const ConfigUpdate& conf);
-
-  // Restore the configuration that was passed to this object's constructor,
-  // overriding any previous calls to `update`.
-  std::vector<ConfigMetadata> reset();
-
   // Return a JSON representation of the current configuration managed by this
   // object.
   nlohmann::json config_json() const;
+
+  std::vector<ConfigMetadata> apply_update(const ConfigManager::Update& conf);
 };
 
 }  // namespace tracing

src/datadog/config_update.h
@@ -1,24 +1,0 @@
-#pragma once
-
-#include <unordered_map>
-
-#include "optional"
-#include "trace_sampler_config.h"
-
-namespace datadog {
-namespace tracing {
-
-// The `ConfigUpdate` struct serves as a container for configuration that can
-// exclusively be changed remotely.
-//
-// Configurations can be `nullopt` to signal the absence of a value from the
-// remote configuration value.
-struct ConfigUpdate {
-  Optional<bool> report_traces;
-  Optional<double> trace_sampling_rate;
-  Optional<std::vector<StringView>> tags;
-  const nlohmann::json* trace_sampling_rules = nullptr;
-};
-
-}  // namespace tracing
-}  // namespace datadog

src/datadog/datadog_agent.cpp
@@ -145,12 +145,14 @@ std::variant<CollectorResponse, std::string> parse_agent_traces_response(
 
 }  // namespace
 
+namespace rc = datadog::remote_config;
+
 DatadogAgent::DatadogAgent(
     const FinalizedDatadogAgentConfig& config,
     const std::shared_ptr<TracerTelemetry>& tracer_telemetry,
     const std::shared_ptr<Logger>& logger,
     const TracerSignature& tracer_signature,
-    const std::shared_ptr<ConfigManager>& config_manager)
+    const std::vector<std::shared_ptr<rc::Listener>>& rc_listeners)
     : tracer_telemetry_(tracer_telemetry),
       clock_(config.clock),
       logger_(logger),
@@ -162,7 +164,7 @@ DatadogAgent::DatadogAgent(
       flush_interval_(config.flush_interval),
       request_timeout_(config.request_timeout),
       shutdown_timeout_(config.shutdown_timeout),
-      remote_config_(tracer_signature, config_manager),
+      remote_config_(tracer_signature, rc_listeners, logger),
       tracer_signature_(tracer_signature) {
   assert(logger_);
   assert(tracer_telemetry_);
@@ -410,14 +412,13 @@ void DatadogAgent::send_heartbeat_and_telemetry() {
   send_telemetry("app-heartbeat", tracer_telemetry_->heartbeat_and_telemetry());
 }
 
-void DatadogAgent::send_app_closing() {
-  send_telemetry("app-closing", tracer_telemetry_->app_closing());
+void DatadogAgent::send_configuration_change() {
+  send_telemetry("app-client-configuration-change",
+                 tracer_telemetry_->configuration_change());
 }
 
-void DatadogAgent::send_configuration_change(
-    const std::vector<ConfigMetadata>& config) {
-  send_telemetry("app-client-configuration-change",
-                 tracer_telemetry_->configuration_change(config));
+void DatadogAgent::send_app_closing() {
+  send_telemetry("app-closing", tracer_telemetry_->app_closing());
 }
 
 void DatadogAgent::get_and_apply_remote_configuration_updates() {
@@ -457,11 +458,13 @@ void DatadogAgent::get_and_apply_remote_configuration_updates() {
         }
 
         if (!response_json.empty()) {
-          auto updated_configuration =
-              remote_config_.process_response(response_json);
-          if (!updated_configuration.empty()) {
-            send_configuration_change(updated_configuration);
-          }
+          remote_config_.process_response(response_json);
+          // NOTE(@dmehala): Not ideal but it mimics the old behavior.
+          // In the future, I would prefer telemetry pushing to the agent
+          // and not the agent pulling from telemetry. That way telemetry will
+          // be more flexible and could support env var to customize how often
+          // it captures metrics.
+          send_configuration_change();
         }
       };
 

commit f0f951848e4f22fb188c8c17e72b584a208493d8

fix: telemetry `configuration_change` payload
Resolve a regression introduced in #130 where the `configuration_change`
payload didn't adhere to the telemetry schema.

src/datadog/tracer_telemetry.cpp
@@ -356,16 +356,12 @@ std::string TracerTelemetry::configuration_change() {
         generate_configuration_field(config_metadata));
   }
 
-  // clang-format off
-  auto configuration_change = nlohmann::json({
-    {"request_type", "app-client-configuration-change"},
-    {"payload", nlohmann::json{
-      {"configuration", configuration_json}
-    }}
-  });
-  // clang-format on
+  auto telemetry_body =
+      generate_telemetry_body("app-client-configuration-change");
+  telemetry_body["payload"] =
+      nlohmann::json{{"configuration", configuration_json}};
 
-  return configuration_change.dump();
+  return telemetry_body.dump();
 }
 
 }  // namespace tracing

test/test_tracer_telemetry.cpp
@@ -14,6 +14,21 @@
 
 using namespace datadog::tracing;
 
+namespace {
+bool is_valid_telemetry_payload(const nlohmann::json& json) {
+  return json.contains("/api_version"_json_pointer) &&
+         json.at("api_version") == "v2" &&
+         json.contains("/seq_id"_json_pointer) &&
+         json.contains("/request_type"_json_pointer) &&
+         json.contains("/tracer_time"_json_pointer) &&
+         json.contains("/runtime_id"_json_pointer) &&
+         json.contains("/payload"_json_pointer) &&
+         json.contains("/application"_json_pointer) &&
+         json.contains("/host"_json_pointer);
+}
+
+}  // namespace
+
 TEST_CASE("Tracer telemetry", "[telemetry]") {
   const std::time_t mock_time = 1672484400;
   const Clock clock = [mock_time]() {
@@ -37,6 +52,7 @@ TEST_CASE("Tracer telemetry", "[telemetry]") {
     SECTION("Without a defined integration") {
       auto app_started_message = tracer_telemetry.app_started({});
       auto app_started = nlohmann::json::parse(app_started_message);
+      REQUIRE(is_valid_telemetry_payload(app_started) == true);
       REQUIRE(app_started["request_type"] == "message-batch");
       REQUIRE(app_started["payload"].size() == 1);
 
@@ -50,6 +66,7 @@ TEST_CASE("Tracer telemetry", "[telemetry]") {
           true, clock, logger, tracer_signature, "nginx", "1.25.2"};
       auto app_started_message = tracer_telemetry.app_started({});
       auto app_started = nlohmann::json::parse(app_started_message);
+      REQUIRE(is_valid_telemetry_payload(app_started) == true);
       REQUIRE(app_started["request_type"] == "message-batch");
       REQUIRE(app_started["payload"].size() == 2);
 
@@ -68,8 +85,8 @@ TEST_CASE("Tracer telemetry", "[telemetry]") {
                           ConfigMetadata::Origin::CODE)}};
 
       auto app_started_message = tracer_telemetry.app_started(configuration);
-
       auto app_started = nlohmann::json::parse(app_started_message);
+      REQUIRE(is_valid_telemetry_payload(app_started) == true);
       REQUIRE(app_started["request_type"] == "message-batch");
       REQUIRE(app_started["payload"].is_array());
       REQUIRE(app_started["payload"].size() == 1);
@@ -97,6 +114,7 @@ TEST_CASE("Tracer telemetry", "[telemetry]") {
           auto config_change_message = nlohmann::json::parse(
               tracer_telemetry.configuration_change(), nullptr, false);
           REQUIRE(config_change_message.is_discarded() == false);
+          REQUIRE(is_valid_telemetry_payload(app_started) == true);
 
           CHECK(config_change_message["request_type"] ==
                 "app-client-configuration-change");
@@ -115,6 +133,7 @@ TEST_CASE("Tracer telemetry", "[telemetry]") {
           auto config_change_message = nlohmann::json::parse(
               tracer_telemetry.configuration_change(), nullptr, false);
           REQUIRE(config_change_message.is_discarded() == false);
+          REQUIRE(is_valid_telemetry_payload(config_change_message) == true);
 
           CHECK(config_change_message["request_type"] ==
                 "app-client-configuration-change");
@@ -149,6 +168,7 @@ TEST_CASE("Tracer telemetry", "[telemetry]") {
   SECTION("generates a heartbeat message") {
     auto heartbeat_message = tracer_telemetry.heartbeat_and_telemetry();
     auto message_batch = nlohmann::json::parse(heartbeat_message);
+    REQUIRE(is_valid_telemetry_payload(message_batch) == true);
     REQUIRE(message_batch["payload"].size() == 1);
     auto heartbeat = message_batch["payload"][0];
     REQUIRE(heartbeat["request_type"] == "app-heartbeat");
@@ -166,6 +186,7 @@ TEST_CASE("Tracer telemetry", "[telemetry]") {
     auto heartbeat_and_telemetry_message =
         tracer_telemetry.heartbeat_and_telemetry();
     auto message_batch = nlohmann::json::parse(heartbeat_and_telemetry_message);
+    REQUIRE(is_valid_telemetry_payload(message_batch) == true);
     REQUIRE(message_batch["payload"].size() == 2);
     auto generate_metrics = message_batch["payload"][1];
     REQUIRE(generate_metrics["request_type"] == "generate-metrics");
@@ -186,6 +207,7 @@ TEST_CASE("Tracer telemetry", "[telemetry]") {
   SECTION("generates an app-closing event") {
     auto app_closing_message = tracer_telemetry.app_closing();
     auto message_batch = nlohmann::json::parse(app_closing_message);
+    REQUIRE(is_valid_telemetry_payload(message_batch) == true);
     REQUIRE(message_batch["payload"].size() == 1);
     auto heartbeat = message_batch["payload"][0];
     REQUIRE(heartbeat["request_type"] == "app-closing");