feaa67ce70bc80f676dabc9cef7501267dc55159
Stephane JANEL <stephane.janel@amadeus.com>
2025-08-05 10:59:50 +0200

    Do not FetchContent nlohmann_json if already found by find_package

--- cmake/deps/json.cmake
+++ cmake/deps/json.cmake
@@ -1,10 +1,14 @@
 include(FetchContent)
 
-set(JSON_BuildTests OFF)
-set(JSON_Install OFF)
+find_package(nlohmann_json CONFIG QUIET)
+if (NOT nlohmann_json_FOUND)
+  set(JSON_BuildTests OFF)
+  set(JSON_Install OFF)
 
-FetchContent_Declare(json
-  URL https://github.com/nlohmann/json/releases/download/v3.11.2/json.tar.xz
-)
+  FetchContent_Declare(nlohmann_json
+    URL https://github.com/nlohmann/json/archive/refs/tags/v3.12.0.tar.gz
+    URL_HASH SHA256=4b92eb0c06d10683f7447ce9406cb97cd4b453be18d7279320f7b2f025c10187
+  )
 
-FetchContent_MakeAvailable(json)
+  FetchContent_MakeAvailable(nlohmann_json)
+endif()
