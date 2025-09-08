0726323fe66910ea8357b22b0c1ee3520cbbf966
Damien Mehala <damien.mehala@datadoghq.com>
2025-09-08 19:24:49 +0200

    chore: bump version to v1.1.0

--- src/datadog/version.cpp
+++ src/datadog/version.cpp
@@ -3,7 +3,7 @@
 namespace datadog {
 namespace tracing {
 
-#define DD_TRACE_VERSION "v1.0.0"
+#define DD_TRACE_VERSION "v1.1.0"
 
 const char* const tracer_version = DD_TRACE_VERSION;
 const char* const tracer_version_string =
