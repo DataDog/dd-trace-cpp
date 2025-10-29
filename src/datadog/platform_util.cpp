5f3b79c10dbe587055b1d5fe1cabb9dec6c20ccb
Damien Mehala <damien.mehala@datadoghq.com>
2025-10-29 11:58:46 -0400

    fix compil ation

--- src/datadog/platform_util.cpp
+++
@@ -1,7 +0,0 @@
-#ifdef _MSC_VER
-#include "platform_util_windows.cpp"
-#elif defined(__APPLE__) || defined(__FreeBSD__)
-#include "platform_util_darwin.cpp"
-#else
-#include "platform_util_unix.cpp"
-#endif
