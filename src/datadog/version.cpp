71ba363abd789127251e25058bb65dcd54e319cd
Xavier Lamorlette <xavier.lamorlette@datadoghq.com>
2026-06-19 11:07:39 +0200

    Bump version following delivery of v2.1.1

--- src/datadog/version.cpp
+++ src/datadog/version.cpp
@@ -2,7 +2,7 @@
 
 namespace datadog::tracing {
 
-#define DD_TRACE_VERSION "v2.1.1"
+#define DD_TRACE_VERSION "v2.1.2"
 
 const char* const tracer_version = DD_TRACE_VERSION;
 const char* const tracer_version_string =
