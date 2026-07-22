a6ed13dab6f3b07291c9f13c75e7771b2fe4a83e
Ivo Anjo <ivo.anjo@datadoghq.com>
2026-07-22 10:46:54 +0000

    Add test for existing memfd datadog-tracer-info publishing logic
    This includes a for-tests-only msgpack decoder dependency.

--- test/test_otel_process_ctx.cpp
+++ test/test_otel_process_ctx.cpp
@@ -1,12 +1,12 @@
 #include <datadog/tracer.h>
 
+#include "common/ctx_sharing_helpers.h"
 #include "mocks/collectors.h"
 #include "mocks/loggers.h"
 #include "otel_process_ctx.h"
 #include "platform_util.h"
 #include "test.h"
 
-namespace fs = std::filesystem;
 using namespace datadog::tracing;
 
 #define OTEL_CTX_TEST(x) TEST_CASE(x, "[otel_process_ctx]")
@@ -23,18 +23,6 @@ std::map<std::string, std::string> to_map(const char** key_value_array) {
   return out;
 }
 
-std::map<std::string, std::string> parse_joined_tags(
-    const std::string& joined_tags) {
-  std::map<std::string, std::string> out;
-  std::istringstream in(joined_tags);
-  for (std::string pair; std::getline(in, pair, ',');) {
-    const auto colon = pair.find(':');
-    REQUIRE(colon != std::string::npos);
-    out.emplace(pair.substr(0, colon), pair.substr(colon + 1));
-  }
-  return out;
-}
-
 }  // namespace
 
 OTEL_CTX_TEST("Tracer construction publishes OTel process context") {
@@ -46,18 +34,6 @@ OTEL_CTX_TEST("Tracer construction publishes OTel process context") {
     expected_container_id = id->value;
   }
   const RuntimeID expected_runtime_id = RuntimeID::generate();
-
-  std::map<std::string, std::string> expected_process_tags = {
-      {"custom.tag", "custom-value"},
-  };
-  expected_process_tags.emplace("entrypoint.name", get_process_name());
-  expected_process_tags.emplace("entrypoint.type", "executable");
-  expected_process_tags.emplace("entrypoint.workdir",
-                                fs::current_path().filename().string());
-  if (auto path = get_process_path()) {
-    expected_process_tags.emplace("entrypoint.basedir",
-                                  path->parent_path().filename().string());
-  }
 
   TracerConfig config;
   config.service = "otel-ctx-svc";
@@ -97,8 +73,8 @@ OTEL_CTX_TEST("Tracer construction publishes OTel process context") {
     const auto extra = to_map(data.extra_attributes);
     REQUIRE(extra.size() == 1);
     REQUIRE(extra.count("datadog.process_tags") == 1);
-    CHECK(parse_joined_tags(extra.at("datadog.process_tags")) ==
-          expected_process_tags);
+    CHECK(datadog::test::parse_joined_tags(extra.at("datadog.process_tags")) ==
+          datadog::test::expected_published_process_tags(config.process_tags));
 
     REQUIRE(otel_process_ctx_read_drop(&read_result));
   }
